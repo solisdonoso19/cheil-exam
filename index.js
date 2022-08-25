6ab92837f2621089dc2f078907e57c1c8ce91f8a
Carlos Ivan Solis Donoso <solisdonoso@gmail.com>
2022-08-25 17:55:46 -0500

    refactorizando endpoints

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 //variable para instalar dependencias
 const express = require("express");
-
+const hotels = require("./controller/hotels");
 const app = express();
 
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.status(200).send("corriendo con exito");
-});
-
-app.post("/", (req, res) => {
-  res.status(201).send("corriendo con exito");
-});
+app.get("/", hotels.list);
+app.post("/", hotels.create);
+app.get("/:id", hotels.get);
+app.put("/:id", hotels.update);
+app.patch("/:id", hotels.update);
+app.delete("/:id", hotels.destroy);
 
 app.listen(port, () => {
-  console.log("Arrancando con exito!");
+  console.log("Corriendo APP");
 });
