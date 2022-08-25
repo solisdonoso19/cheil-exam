e4b3a539b9c6e4b7e7902c3d06cc685965cafab6
Carlos Ivan Solis Donoso <solisdonoso@gmail.com>
2022-08-25 18:15:20 -0500

    Terminanado refactorizacion

---
+++ controller/hotels.js
@@ -0,0 +1,19 @@
+const Hotels = {
+  get: (req, res) => {
+    res.status(200).send("Este es un Hotel!");
+  },
+  list: (req, res) => {
+    res.status(200).send("Listando...");
+  },
+  create: (req, res) => {
+    res.status(201).send("Creando...");
+  },
+  update: (req, res) => {
+    res.status(204).send("Actualizando...");
+  },
+  destroy: (req, res) => {
+    res.status(204).send("Eliminando...");
+  },
+};
+
+module.exports = Hotels;
