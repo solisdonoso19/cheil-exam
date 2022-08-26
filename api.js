0a81b8dab888c9f2a3e73a511126a4a80847c5ec
Carlos Ivan Solis Donoso <solisdonoso@gmial.com>
2022-08-26 11:11:46 -0500

    Terminando el modelado de la api

--- api.js
+++ api.js
@@ -6,17 +6,18 @@ port  : puerto donde correra el proyecto en local
 */
 
 const express = require("express");
-const hotels = require("./controller/hotels");
+const endpoints = require("./controller/endpoints");
 const app = express();
-
 const port = 3000;
+app.use(express.json());
+
 //llamando a los endpoints
-app.get("/", hotels.list);
-app.post("/", hotels.create);
-app.get("/:id", hotels.get);
-app.put("/:id", hotels.update);
-app.patch("/:id", hotels.update);
-app.delete("/:id", hotels.destroy);
+app.get("/", endpoints.list);
+app.post("/", endpoints.create);
+app.get("/:id", endpoints.get);
+app.put("/:id", endpoints.update);
+app.patch("/:id", endpoints.update);
+app.delete("/:id", endpoints.destroy);
 
 app.get("*", (req, res) => {
   res.status(404).send("Esta Pagina no existe");
--- controller/hotels.js
+++ controller/hotels.js
@@ -1,21 +1,63 @@
-//endpoint de la api
+const mongoose = require("mongoose"); //instancia de libreria mongoose
+//conexion a la base de datos
+try {
+  mongoose.connect(
+    "mongodb+srv://admin:admin@cheil-api.xqhtidq.mongodb.net/?retryWrites=true&w=majority"
+  );
+} catch (error) {
+  console.log("Error al conectarse a la base de datos." + error);
+}
 
-const Hotels = {
-  get: (req, res) => {
-    res.status(200).send("Este es un Hotel!");
+//creando el modelo en MongoDB
+const Hotel = mongoose.model("Hotel", {
+  //id: Number,
+  name: { type: String, required: true, minLength: 4 },
+  category: { type: String, required: true, maxLength: 1 },
+  price: { type: Number, required: true },
+  images: {
+    img1: String,
+    img2: String,
+    img3: String,
   },
-  list: (req, res) => {
-    res.status(200).send("Listando...");
+  reviews: {
+    points: { type: Number, required: true, maxLength: 1 },
+    comment: { type: String, maxLength: 255 },
   },
-  create: (req, res) => {
-    res.status(201).send("Creando...");
-  },
-  update: (req, res) => {
-    res.status(204).send("Actualizando...");
-  },
-  destroy: (req, res) => {
-    res.status(204).send("Eliminando...");
-  },
+});
+module.exports = Hotel;
+
+const create = async () => {
+  const hotel = new Hotel({
+    name: "Miramar",
+    category: "5 Estrellas",
+    price: 50,
+    images: {
+      img1: "imagen 1",
+      img2: "imagen 2",
+      img3: "imagen 3",
+    },
+    reviews: {
+      points: 5,
+      comment: "Bonito hotel",
+    },
+  });
+
+  try {
+    const saveHotel = await hotel.save();
+    console.log(saveHotel);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-module.exports = Hotels;
+//create();
+const searchAll = async () => {
+  try {
+    const hotels = await Hotel.find();
+    console.log(hotels);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+searchAll();
